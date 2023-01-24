17a475d6a298dae2e6ed3c6475f0ac2dcf4dd384
Artur-Panasiuk <47883583+Artur-Panasiuk@users.noreply.github.com>
2023-01-24 16:17:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,64 @@
 #include <iostream>
 #include <complex>
+#define _USE_MATH_DEFINES
 #include "math.h"
 #include <time.h>
+#include <memory>
 
 using namespace std;
 
 const int MAX_ORDER = 13;
 const bool PRINT_COEFS = false;
+const std::complex<double> constI(0, 1);
+const double constE = exp(1.0);
 
 
 template <typename A>
 complex<A>* dft(A arr[], const int arrSize){
-    double r[arrSize];
-    double j[arrSize];
+    complex<A>* result = new complex<A>[arrSize];
     for(int i = 0; i < arrSize; i++){
-
+        double rel = 0;
+        double com = 0;
+        for(int j = 0; j < arrSize; j++){
+            double alpha = 2 * M_PI * i * j / arrSize;
+            rel += arr[j] * cos(alpha);
+            com += arr[j] * sin(alpha);
+        }
+        complex<A> temp(rel, com);
+        result[i] = temp;
     }
+    return result;
 }
 
 template <typename B>
-complex<B>* fft(B arr[], int arrSize){
+complex<B>* fft(B arr[], const int arrSize){
+    complex<B>* store = new complex<B>[arrSize];
 
+    for(int i = 0; i < arrSize; i++){
+        double alpha = -2 * M_PI * i / arrSize;
+        double rel = cos(alpha), com = sin(alpha);
+        complex<B> temp(rel, com);
+        store[i] = temp;
+    }
+
+    complex<B>* x0 = new complex<B>[arrSize/2];
+    complex<B>* x1 = new complex<B>[arrSize/2];
+
+    for(int i = 0; i < arrSize/2; i++){
+        x0[i] = arr[i*2];
+        x1[i] = arr[i*2 + 1];
+    }
+
+    complex<B>* y0 = fft(x0, arrSize/2);
+    complex<B>* y1 = fft(x1, arrSize/2);
+
+    complex<B>* result = new complex<B>[arrSize];
+
+    for(int i = 0; i < arrSize/2; i++){
+        result[i] = y0[i] + store[i] * y1[i];
+        result[i + arrSize / 2] = y0[i] - store[i] * y1[i];
+    }
+    return result;
 }
 
 int main()
@@ -50,7 +88,7 @@ int main()
 //
 //        if(PRINT_COEFS){
 //            for(int k = 0; k < N; k++){
-//                //wypis na ekran wspolczynnikow obu transformat ( czesci rzeczywiste i urojone )
+//                  cout << "x" << k << ": DFT " << cDFT[k] << " FFT " << cFFT[i] << endl;
 //            }
 //        }
 //        delete[] f;
@@ -63,13 +101,16 @@ int main()
     const int N = 1 << 2;
 
     double* f = new double[N];
-    for(int n = 0; n < N; n++){
+    for(int n = 1; n < N; n++){
         f[n] = n / (double)N;
-        cout << f[n]<<endl;
     }
-    complex<double>* cDFT = dft(f, N);
+    complex<double>* cFFT = fft(f, N);
 
-    delete[] cDFT;
+    for(int i = 1; i < N; i++){
+        cout << f[i] << ": ( " << cFFT[i].real() << ", " << cFFT[i].imag() << "i )" << endl;
+    }
+
+    delete[] cFFT;
 
     return 0;
 }
